63e64eaf7093be13a2c1db1f6d468352a18c204c
Farukh Iskalinov <iskalinov133@gmail.com>
2022-04-16 17:52:43 +0600

    Initial Commit

---
+++ One-Lab-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  One-Lab-3
+//
+//  Created by Farukh on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
